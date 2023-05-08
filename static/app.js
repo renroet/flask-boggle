9ff6f474bc067b69a0a7ad4ee4f0a2be43d00f6a
Ren Roetzel <renroetzel@gmail.com>
2023-05-08 15:35:48 -0400

    clean-up

--- static/app.js
+++ static/app.js
@@ -5,12 +5,6 @@ const $form = $('#guess')
 const $msg = $('#result')
 const $scoreText = $('#score-text')
 const $game = $('#game')
-// const $restart = $('.restart')
-
-// $restart.on('click', function() {
-//     location.reload()})
-
-// $guess = $form.value()
 
 
 class BoggleGame {
@@ -25,11 +19,7 @@ class BoggleGame {
         this.start.on('click', this.handleSubmit.bind(this))
     }
 
-    // showFinalScore() {
-    //     const $final = $('#final-score').append(`<p class="final">${this.score}</p>`)
-    //     $('#final-score').show()
-    // }
-
+    
     timer() {
         if(this.secs > 0 ) {
             this.secs -= 1 
@@ -47,12 +37,7 @@ class BoggleGame {
         const $final = $('#final-score').append(`<p class="final">${this.score}
         <form action="/set-up" method="POST" class="restart"> <button>RESTART</button></form></p>`)
         
-        // const res = await axios.get('/high_score', {params: {score: score}})
-        // const hiScore = result.data
-        // if(score > hiScore) {
-        //     this.updateHighScore(score)
-        //     $final.append('<p class="final">HIGH SCORE!</p>')
-        // }
+        
         const res = await axios.post('/high_score', {score: score})
         if (res.data.newHighScore) {
             $final.append('<p class="high-score">HIGH SCORE!</p>')
@@ -61,23 +46,6 @@ class BoggleGame {
     }
 
     
-
-    // async updateHighScore(score) {
-    //     const res = await axios.post('/high_score', {score: score})
-    //     if (newHighScore) {
-
-    //     }
-    // }
-
-    // $.ajax({
-    //     type: 'POST',
-    //     url: '/process',
-    //     data: JSON.stringify(value_data),
-    //     contentType: 'application/json',
-    //     success: function (response_data) {
-    //         alert("success");
-    //     }   
-    // });
 
     updateScore(word) {
         let points = word.length
@@ -118,42 +86,8 @@ class BoggleGame {
         $('input:text').val('') 
     }
     
-    // $formSubmit.on('click', handleSubmit)
 
 }
 
 const myBoggle = new BoggleGame($game, secs = 60)
-
-
-
-
-
-
-
-
-
-// async function handleSubmit(e) {
-//     e.preventDefault()
-//     word = $('input:text').val()
-//     if(checkSeen(word)) {
-//         $msg.text('Word already submitted')
-//         return 
-//     }
-//     else {
-//         res = await axios.get('/check', {params: {word: word}})
-//         result = res.data['result']
-//         notifyPlayer(result)
-//         if(result === 'ok') {
-//             updateSeen(word)
-//             updateScore(word)
-//         }
-//     }
-// }
-
-
-
-// $formSubmit.on('click', handleSubmit)
-
-// probably put a timer on the event listener to disable any more clicks
-
 
